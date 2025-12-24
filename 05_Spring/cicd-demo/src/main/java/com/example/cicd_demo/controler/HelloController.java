89e09f61194bb5a8a1c8ec8e81beff159269182a
Ratnmala1007 <ratnamalas1007@gmail.com>
2025-12-24 15:42:31 +0530

    Fix CI/CD SSH restart issue

--- 05_Spring/cicd-demo/src/main/java/com/example/cicd_demo/controler/HelloController.java
+++ 05_Spring/cicd-demo/src/main/java/com/example/cicd_demo/controler/HelloController.java
@@ -3,7 +3,7 @@ package com.example.cicd_demo.controler;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//controller
 @RestController
 public class HelloController {
 
